881d2aa13b92e6789cfea6d3f673686d850a577c
DevLepsy <70275748+DevLepsy@users.noreply.github.com>
2022-01-31 23:12:54 +0100

    add bar

--- src/Pages/DashBoardFinance/DashBoardFinance.js
+++ src/Pages/DashBoardFinance/DashBoardFinance.js
@@ -1,5 +1,26 @@
-import React from 'react';
+import React , {useState, useContext} from 'react';
+import './DashBoardFinance.css'
+import BarChart from '../../Components/AllChart/BarChart'
+
 
 export default function DashBoardFinance() {
-  return <div></div>;
+  return (
+    <div className="global-container">
+        <h1>Les résultats de l'année : 2020</h1>
+        <form>
+            <label htmlFor="year">Choisissez une date</label>
+            <select id="year">
+                <option value="2021">2021</option>
+                <option value="2020">2020</option>
+                <option value="2019">2019</option>
+                <option value="2018">2018</option>
+            </select>
+        </form>
+        <div className="dashboard-container">
+            <BarChart />
+        </div>
+
+    </div>
+
+  );
 }
