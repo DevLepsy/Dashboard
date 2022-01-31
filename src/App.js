e178c3e60187ca92a5cd20f2a81baeeca4cefc95
DevLepsy <70275748+DevLepsy@users.noreply.github.com>
2022-01-31 22:25:21 +0100

    add create function app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import DashBoardFinance from "./Pages/DashBoardFinance/DashBoardFinance";
+import DashBoardEmployees from "./Pages/DashBoardEmployees/DashBoardEmployees";
+import Sidebar from "./Components/SideBar/SideBar";
+import { Routes, Route } from "react-router-dom";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Sidebar />
+    <Routes>
+      
+      <Route path="/" element={<DashBoardFinance />} />
+      <Route path="DashBoardEmployees" element={<DashBoardEmployees />} />
+    </Routes>
     </div>
   );
 }
-
-export default App;
